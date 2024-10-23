625e4a59310c254d8606e0911973f228ecac32fb
kevinkandie <kevinkan500@gmail.com>
2024-10-23 18:28:54 +0300

    added switch case stamements in index.js.

--- index.js
+++ index.js
@@ -6,20 +6,30 @@ function distanceFromHqInBlocks(pickupLocation) {
   function distanceFromHqInFeet(pickupLocation) {
     const blocks = distanceFromHqInBlocks(pickupLocation);
     return blocks * 264; }
-    function distanceTravelledInFeet(start, destination) {
-        const blocks = Math.abs(destination - start);
-        return blocks * 264;
+    function distanceTravelledInFeet(start, destination){
+      switch(true){
+        case(start<destination):
+        return (destination-start)*264;
+        default:
+          return(start-destination)*264;
       }
+    
+      }
+
+    
       function calculatesFarePrice(start, destination) {
-        const distance = distanceTravelledInFeet(start, destination);
-      
-        if (distance <= 400) {
-          return 0; // Free ride
-        } else if (distance > 400 && distance <= 2000) {
-          return (distance - 400) * 0.02; // Charge $0.02 per foot after the first 400 feet
-        } else if (distance > 2000 && distance <= 2500) {
-          return 25; // Flat fare for distances between 2000 and 2500 feet
-        } else {
-          return 'cannot travel that far'; // Over 2500 feet
+        const  distance = distanceTravelledInFeet(start,destination);
+        switch(true){
+          case (distance <= 400):
+            return 0; 
+          case (distance > 400 && distance <= 2000):
+          return (distance-400)*0.02;
+      case (distance > 2000 && distance <= 2500):
+        return 25;
+
+        default:
+          return `cannot travel that far`
         }
       }
+        
+
